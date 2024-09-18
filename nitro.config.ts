01d4c01d31e937c9365a2f11698ab8be73b8c3f4
mjdNjhNJ <your_email@example.com>
2024-09-18 13:40:04 +0200

    feat: add Sentry source map support

--- nitro.config.ts
+++ nitro.config.ts
@@ -1,4 +1,14 @@
+import { sentryRollupPlugin } from "@sentry/rollup-plugin";
+
 //https://nitro.unjs.io/config
 export default defineNitroConfig({
-  srcDir: "server"
+  sourceMap: true,
+  srcDir: "server",
+  plugins: [
+    sentryRollupPlugin({
+      authToken: process.env.SENTRY_AUTH_TOKEN,
+      org: "REDACTED",
+      project: "REDACTED",
+    }),
+  ],
 });
